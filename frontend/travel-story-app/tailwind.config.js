629cf65d345d536541ff192760dcb103e28e32d9
Khushi Mahto <148677058+KhushiMahto@users.noreply.github.com>
2025-07-06 03:15:34 +0530

    Update tailwind.config.js

--- frontend/travel-story-app/tailwind.config.js
+++ frontend/travel-story-app/tailwind.config.js
@@ -15,8 +15,8 @@ export default {
         secondary: "#EF863E",
       },
       backgroundImage: {
-        'login-bg-img': "url('/public/bg-image.jpg')",
-        'signup-bg-img': "url('/public/signup-bg-img.jpg')",
+        'login-bg-img': "url('/bg-image.jpg')",
+        'signup-bg-img': "url('/signup-bg-img.jpg')",
       }
     },
   },
